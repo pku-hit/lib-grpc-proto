589438d41aca949b003bf69cf306e2b590e58ac1
GaoYu <solonot@163.com>
2019-09-06 13:28:56 +0800

    更新构建脚本

--- genJava.sh
+++ genJava.sh
@@ -1,2 +1,4 @@
-mkdir -p src/main/java
-protoc --plugin=protoc-gen-grpc-java=/usr/local/bin/protoc-gen-grpc-java.exe --grpc-java_out=./src/main/java --java_out=./src/main/java  *.proto
+GEN_FOLDER=src/main/java
+mkdir -p $GEN_FOLDER
+protoc -I$GOPATH/src -I. --plugin=protoc-gen-grpc-java=/usr/local/bin/protoc-gen-grpc-java.exe --grpc-java_out=. --java_out=$GEN_FOLDER  *.proto
+mvn package
